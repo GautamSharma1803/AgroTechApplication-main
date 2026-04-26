30f2ec6ede53ddede8843b8705e59ffa48e964fa
Gautam Sharma <gautamshar453@gmail.com>
2026-04-26 19:52:47 +0530

    effect fix

--- src/app/pages/MarketPage.tsx
+++ src/app/pages/MarketPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState ,useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ImageWithFallback } from '../components/figma/ImageWithFallback';
 import { market } from '../utils/api';
